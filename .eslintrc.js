edac16abd5ff65b6236ba8c2302d6d0d0813c5eb
rlawogus65@gmail.com <rlawogus65@gmail.com>
2024-11-01 17:51:56 +0900

    Modify Eslint

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:vue/essential',
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+    requireConfigFile: false,
+  },
+  plugins: [
+    'vue',
+  ],
+  rules: {
+    indent: ['error', 2],
+    quotes: ['error', 'single'],
+    'quote-props': ['error', 'as-needed'],
+    semi: ['error', 'always'], // 세미콜론을 항상 추가
+    'comma-dangle': ['error', 'always-multiline'], // 여러 줄에서 쉼표 필요
+  },
+};
