ff4b69f30a80fd561a78347eaf6c3a167e65f9c5
rlawogus65@gmail.com <rlawogus65@gmail.com>
2024-11-27 21:18:58 +0900

    Add background.jpg

--- src/App.vue
+++ src/App.vue
@@ -23,3 +23,17 @@ export default {
   name: 'App',
 };
 </script>
+
+<style scoped>
+#app {
+  background-image: url('@/assets/background.jpg');
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-attachment: fixed;
+  min-height: 100vh;
+  width: 100%;
+  height: auto;
+  background-color: #000;
+}
+</style>
